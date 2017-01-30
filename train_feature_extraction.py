2f91ab2d363788cc50b7b9a4707c75de61fc12a2
Tse Kit Yam <me@kytse.com>
2017-01-30 18:32:00 +0800

    Delete the usage of variable `keep_prob`

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -59,7 +59,7 @@ def evaluate(X_data, y_data):
     sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
